632009f0f5112190a6e2777114e8c1b4605c9420
yiheinchai <76833604+yiheinchai@users.noreply.github.com>
2021-02-10 01:07:15 +0800

    working navbar

--- js/script.js
+++ js/script.js
@@ -16,6 +16,16 @@
 //}
 //
 //
+$(function() {
+    $("#navbar-toggler").blur(function (event) {
+        var screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+            $("#navbarNav").collapse('hide');
+        }
+    });
+});
+
+
 function showApps () {
     var name = document.getElementById("name").value;
     var industry = document.getElementById("industry").value;
